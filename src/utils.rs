130b52c3ddecd4af041fd1f9d8d02d8c0e47e8c9
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-10-06 10:43:19 +0100

    Exit cleanly on Ctrl+D (EOF)

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,31 @@
-use std::io;
- use std::io::Write;
+use std::io::{self, Write};
 
 pub fn input_loop() {
     'main: loop {
-        print!("$");
+        print!("$ ");
         io::stdout().flush().unwrap();
-        let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).unwrap();
-        let input = user_input.trim();
-        if input == "exit" {
-            break 'main;
-        }
 
+        let mut user_input = String::new();
+        let bytes_read = io::stdin().read_line(&mut user_input);
+
+        match bytes_read {
+            Ok(0) => {
+
+                break 'main;
+            }
+            Ok(_) => {
+                let input = user_input.trim();
+
+                if input == "exit" {
+                    break 'main;
+                }
+
+                println!("You entered: {}", input);
+            }
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                break 'main;
+            }
+        }
     }
 }
