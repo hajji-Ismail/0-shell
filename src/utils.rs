a3d40b6a5426a96b54a3baf789d21d9b92200b5d
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-10-06 10:25:21 +0100

    Implement a continuous input loop

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,13 @@
+use std::io;
+ use std::io::Write;
+pub fn input_loop() {
+    'main: loop {
+        print!("$");
+        io::stdout().flush().unwrap();
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).unwrap();
+        let input = user_input.trim();
 
+        println!("${input}");
+    }
+}
