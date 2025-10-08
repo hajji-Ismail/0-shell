ed7c0d9329a3bf86c650a85b66c9b73040caced3
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-10-08 14:50:44 +0100

    deleting the unused function

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -1,55 +1,6 @@
 use crate::utils::Parsing;
 use std::fs;
 
-pub fn ls1(parsing: Parsing) {
-    if parsing.flag.is_empty() {
-        let paths = if parsing.arg.is_empty() {
-            vec![".".to_string()]
-        } else {
-            parsing.arg.clone()
-        };
-
-        for path in paths.iter() {
-            if paths.len() > 1 {
-                println!("{}:", path);
-            }
-
-            match fs::read_dir(path) {
-                Ok(entries_iter) => {
-                    let mut entries: Vec<_> = entries_iter.filter_map(|e| e.ok()).collect();
-
-                    entries.sort_by_key(|e| e.file_name());
-
-                    for entry in entries {
-                        let name = entry.file_name().to_string_lossy().into_owned();
-
-                        if !name.starts_with('.') {
-                            println!("{}", name);
-                        }
-                    }
-                }
-                Err(err) => {
-                    eprintln!("ls: cannot access '{}': {}", path, err);
-                }
-            }
-
-            if paths.len() > 1 {
-                println!();
-            }
-        }
-    } else {
-        let flag_tuple = flag(parsing.flag);
-        match flag_tuple {
-            Err(e) => {
-                println!("{e}")
-            }
-            Ok(res) => {
-                println!("{:?}", res)
-            }
-        }
-    }
-}
-
 fn flag(flags: Vec<String>) -> Result<(bool, bool, bool), String> {
     let mut all = false;
     let mut long = false;
