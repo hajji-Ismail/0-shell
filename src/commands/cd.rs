a736b972974789af098cc8251b015fd179a928a3
helouaziz <ouazizi2code@gmail.com>
2025-10-13 17:46:29 +0100

    not working with default

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -4,8 +4,13 @@ use std::path::Path;
 pub fn cd(path: Option<&str>) {
     let target = match path {
         Some(p) => Path::new(p).to_path_buf(),
-        None => Path::new("~").to_path_buf()
+        None => match env::var("HOME") {
+            Ok(home) => Path::new(&home).to_path_buf(),
+            Err(_) => Path::new("/").to_path_buf(), 
+        },
     };
+
+    println!("{:?}", target);
 
     if let Err(e) = env::set_current_dir(&target) {
         eprintln!("cd: {}", e);
