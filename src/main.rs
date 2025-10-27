0efea7cec02f32d018e521392eeaa32d69d54eb6
helouazizi <ouazizi2code@gmail.com>
2025-10-27 10:57:30 +0100

    add c-shell

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 mod utils;
 mod parser;
 mod commands;
+
 use std::process;
-use std::{ io::{ self, Write }};
-use utils::{ input_loop };
+use std::io::{ self, Write };
+use utils::input_loop;
 
 fn main() {
-    print_welcome_message();
+    if let Err(_) = print_welcome_message() {
+        process::exit(0);
+    }
+
     input_loop();
 }
 
-fn print_welcome_message() {
+fn print_welcome_message() -> io::Result<()> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
-    if
-        let Err(_) = writeln!(
-            handle,"test"
-        )
-    {
-      process::exit(0);
-    }
+    write!(
+        handle,
+        "
+██████╗  ██████╗     ███████╗██╗  ██╗███████╗██╗     ██╗     
+██╔══██╗██╔═══██╗    ██╔════╝██║  ██║██╔════╝██║     ██║     
+██║  ██║██║   ██║    ███████╗███████║█████╗  ██║     ██║     
+██║  ██║██║   ██║    ╚════██║██╔══██║██╔══╝  ██║     ██║     
+██████╔╝╚██████╔╝    ███████║██║  ██║███████╗███████╗███████╗
+╚═════╝  ╚═════╝     ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
+                                      
+"
+    )
 }
